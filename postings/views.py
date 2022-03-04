0969673fdbd954713e3e45d02def451c13fafbd9
melaniebeni <61175120+melaniebeni@users.noreply.github.com>
2022-03-04 23:59:18 +0000

    final

--- postings/views.py
+++ postings/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Internship
 from .forms import InternshipForm
 from django.views.decorators.csrf import csrf_exempt
 
@@ -12,8 +13,11 @@ def home(request):
 
 
 def posts(request):
-    context = {}
-    return render(request, 'postings/posts.html', context)
+    internships = Internship.objects
+    # context = {
+    #   'internshps': internships
+    # }
+    return render(request, 'postings/posts.html', {'internships': internships})
 
 
 @csrf_exempt
@@ -28,6 +32,17 @@ def create(request):
         'internshipform': new_form,
         'note': note,
       }
+
+      
+      internshipname = request.POST['internship_name']
+      employername = request.POST['company_name']
+      link = request.POST['link']
+      description = request.POST['description']
+      new_post = Internship(internship_name=internshipname, employer_name=employername, link=link, description=description)
+      
+      new_post.save()
+
+      
       return render(request, 'postings/create.html', context)
   else:
     form = InternshipForm()
