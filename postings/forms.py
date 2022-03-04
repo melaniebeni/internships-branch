638bbd9166ae51db9b43c704c015c2b086bd5249
melaniebeni <61175120+melaniebeni@users.noreply.github.com>
2022-03-04 22:51:35 +0000

    form submission

--- postings/forms.py
+++ postings/forms.py
@@ -2,4 +2,6 @@ from django import forms
 
 class InternshipForm(forms.Form):
   internship_name = forms.CharField(label="Internship Name", max_length=30)
-  company_name = forms.CharField(label=)
+  company_name = forms.CharField(label="Company Name", max_length=30)
+  link = forms.CharField(label="Link", max_length=200)
+  description = forms.CharField(label="Description", max_length=200)
--- postings/views.py
+++ postings/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import InternshipForm
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
@@ -14,6 +16,22 @@ def posts(request):
     return render(request, 'postings/posts.html', context)
 
 
+@csrf_exempt
 def create(request):
-  context = {}
-  return render(request, 'postings/create.html', context)
+
+  if request.method == 'POST':
+    filled_form = InternshipForm(request.POST)
+    if filled_form.is_valid():
+      note = 'Thanks! Your Post Has Been Saved'
+      new_form = InternshipForm()
+      context = {
+        'internshipform': new_form,
+        'note': note,
+      }
+      return render(request, 'postings/create.html', context)
+  else:
+    form = InternshipForm()
+    context = {
+      'internshipform': form
+    }
+    return render(request, 'postings/create.html', context)
